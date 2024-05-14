80c4faa20240d90483cb4d4067a1b8dd9759dc31
Tomel Shaji <tomelshaji22@gmail.com>
2024-05-14 19:19:35 +0530

    "index cluster updated"

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/retrokit");
+// mongoose.connect("mongodb://127.0.0.1:27017/retrokit");
+mongoose.connect("mongodb+srv://tomelshaji22:r1TEztxRJAPhq5SX@retrokit.p1xt8dt.mongodb.net/");
+
+
 const path = require('path');
 
 
