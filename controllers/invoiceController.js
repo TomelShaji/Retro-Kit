065099aac80c5f0bbf363b4d7f7b58e8984f2599
Tomel Shaji <tomelshaji22@gmail.com>
2024-05-21 17:12:53 +0530

    removed puppetteer code

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -14,47 +14,6 @@ const path = require('path');
 
 const ejs = require('ejs');
 
-// const createInvoice = async (req, res) => {
-//     try {
-//         const orderId = req.params.orderId;
-
-//         // Fetch order details from the database
-//         const order = await Order.findById(orderId).populate('userId', 'name email phone').populate('products.productId').exec();
-        
-//         if (!order) {
-//             return res.status(404).send('Order not found');
-//         }
-
-//         // Render invoice template with order data
-//         const invoiceHtml = await ejs.renderFile('views/users/invoice.ejs', { order });
-
-//         // Launch headless Chrome browser
-//         const browser = await puppeteer.launch();
-
-//         // Create a new page
-//         const page = await browser.newPage();
-
-//         // Set content for the PDF invoice
-//         await page.setContent(invoiceHtml);
-
-//         // Generate PDF with Puppeteer
-//         const pdfBuffer = await page.pdf({ format: 'A4' });
-
-//         // Close the browser
-//         await browser.close();
-
-//         // Set content type and disposition for PDF
-//         res.setHeader('Content-Type', 'application/pdf');
-//         res.setHeader('Content-Disposition', `attachment; filename=invoice_${orderId}.pdf`);
-
-//         // Send the PDF buffer as response
-//         res.send(pdfBuffer);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-
 const createInvoice = async (req, res) => {
     try {
         const orderId = req.params.orderId;
